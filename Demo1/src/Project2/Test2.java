bfa874e3a9049c2f04bea4d39ea0243729ad6836
abhi <abhi@abhi-PC>
2021-12-21 10:14:59 +0530

    update

--- Demo1/src/Project2/Test2.java
+++ Demo1/src/Project2/Test2.java
@@ -3,19 +3,25 @@ package Project2;
 public class Test2 {
 
 	public static void main(String[] args) {
+		
+		Test2 t=new Test2();
+		t.m1();
+		t.m2();
 		System.out.println("demo test2");
 		System.out.println("demo test2 push");
+	}
 		
-		public void m1(){
-		System.out.println("m1 method");
-		
-		
+		public void m1() {
+			System.out.println("m1 method");
+			
+			
+			
 		}
-		public void m2(){
-		System.out.println("m2 method");
-		
+		public void m2() {
+			
+			System.out.println("m2 method ");
+			
 		}
 
-	}
 
 }
